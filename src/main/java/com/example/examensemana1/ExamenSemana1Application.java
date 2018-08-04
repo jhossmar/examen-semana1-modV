9f159addfa3b1d4b9623070da040c1e816c96388
jhossmar <jhosmar_ramos@hotmail.com>
2018-08-03 22:46:47 -0400

    adicionando mensaje '<<<MUNDO>>>'

--- src/main/java/com/example/examensemana1/ExamenSemana1Application.java
+++ src/main/java/com/example/examensemana1/ExamenSemana1Application.java
@@ -9,5 +9,6 @@ public class ExamenSemana1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(ExamenSemana1Application.class, args);
 		System.out.println("<<<<<hola>>>>>");
+		System.out.println("<<<MUNDO>>>>>>>");
 	}
 }
