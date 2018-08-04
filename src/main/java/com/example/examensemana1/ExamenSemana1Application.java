d799d3c95fd4321e64b03cb04bd70a9171962532
jhossmar <jhosmar_ramos@hotmail.com>
2018-08-03 22:44:02 -0400

    proyecto spring con mensaje 'hola'

---
+++ src/main/java/com/example/examensemana1/ExamenSemana1Application.java
@@ -0,0 +1,13 @@
+package com.example.examensemana1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenSemana1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenSemana1Application.class, args);
+		System.out.println("<<<<<hola>>>>>");
+	}
+}
